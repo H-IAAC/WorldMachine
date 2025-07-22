2d6109b831e5a888b004ecf801b3fdfecda1ce08
Elton Cardoso do Nascimento <43186596+EltonCN@users.noreply.github.com>
2025-07-22 16:27:10 -0300

    WM Inference method

--- src/world_machine/world_machine.py
+++ src/world_machine/world_machine.py
@@ -6,6 +6,20 @@ from world_machine.layers.positional_encoder import create_positional_encoder
 from .layers import Clamp, LTanh, Sine, SinTanh
 
 
+def generate_sensorial_masks(sensorial_data: TensorDict) -> TensorDict:
+    batch_size = sensorial_data.shape[0]
+    seq_len = sensorial_data.shape[1]
+    device = sensorial_data.device
+
+    sensorial_masks = TensorDict(
+        device=device, batch_size=[batch_size, seq_len])
+    for name in sensorial_data.keys():
+        sensorial_masks[name] = torch.ones(
+            (batch_size, seq_len), dtype=bool, device=device)
+
+    return sensorial_masks
+
+
 class WorldMachine(torch.nn.Module):
     def __init__(self, state_size: int, max_context_size: int,
                  blocks: torch.nn.ModuleList,
@@ -94,18 +108,17 @@ class WorldMachine(torch.nn.Module):
             seq_len = input_sequence_size
 
         if sensorial_data is None:
-            sensorial_data = TensorDict(device=device)
+            sensorial_data = TensorDict(
+                device=device, batch_size=[batch_size, seq_len])
+        elif sensorial_data.shape[1] != seq_len:
+            sensorial_data = sensorial_data[:, :seq_len]
 
         if sensorial_masks is None:
-            sensorial_masks = TensorDict(device=device, batch_size=batch_size)
-            for name in sensorial_data.keys():
-                sensorial_masks[name] = torch.ones(
-                    (batch_size, seq_len), dtype=bool, device=device)
+            sensorial_masks = generate_sensorial_masks(sensorial_data)
+        if sensorial_masks.shape[1] != seq_len:
+            sensorial_masks = sensorial_masks[:, :seq_len]
 
         x: TensorDict = sensorial_data.clone()
-
-        for dim in x.keys():
-            x[dim] = x[dim][:, :seq_len]
 
         # State encoding
         if state_decoded is not None:
@@ -160,5 +173,79 @@ class WorldMachine(torch.nn.Module):
                  state_decoded: torch.Tensor | None = None,
                  sensorial_data: TensorDict | None = None,
                  sensorial_masks: TensorDict | None = None,
-                 input_sequence_size: int | None = None):
+                 input_sequence_size: int | None = None) -> TensorDict:
         return super().__call__(state, state_decoded, sensorial_data, sensorial_masks, input_sequence_size)
+
+    def inference(self,
+                  state: torch.Tensor,
+                  sensorial_data: TensorDict | None = None,
+                  sensorial_masks: TensorDict | None = None,
+                  start: int = 0,
+                  total_size: int | None = None,
+                  replace_sensorial_data: bool = False) -> TensorDict:
+        '''
+        Autoregressive inference.
+
+        Args:
+            state (torch.Tensor): The initial state tensor.
+            sensorial_data (TensorDict | None, optional): Input sensorial data. Defaults to None.
+            sensorial_masks (TensorDict | None, optional): Output sensorial data. Defaults to None.
+            start (int, optional): Index to start the inference. Defaults to 0.
+            total_size (int | None, optional): Total size of the sequence, if None, uses the state sequence length. Defaults to None.
+
+        Returns:
+            TensorDict: _description_
+        '''
+
+        device = next(iter(self.parameters())).device
+        batch_size = state.shape[0]
+
+        if total_size is None:
+            total_size = state.shape[1]
+
+        elif state.shape != total_size:
+            expansion = torch.empty([state.shape[0], total_size-state.shape[1],
+                                    state.shape[2]], dtype=state.dtype, device=state.device)
+            state = torch.hstack([state, expansion])
+
+        expansion_seq_len = 0
+        if sensorial_data is None:
+            sensorial_data = TensorDict(device=device, batch_size=[
+                                        batch_size, total_size])
+        elif sensorial_data.batch_size[1] != total_size:
+            expansion_seq_len = total_size-sensorial_data.batch_size[1]
+            expansion = TensorDict(device=device, batch_size=[
+                                   batch_size, expansion_seq_len])
+
+            for key in sensorial_data.keys():
+                expansion[key] = torch.empty(
+                    [batch_size, expansion_seq_len]+list(sensorial_data[key].shape[2:]), device=device)
+
+            sensorial_data = torch.cat([sensorial_data, expansion], dim=1)
+
+        if sensorial_masks is None:
+            sensorial_masks = generate_sensorial_masks(sensorial_data)
+
+            if expansion_seq_len != 0:
+                for key in sensorial_data.keys():
+                    sensorial_masks[key][:, -expansion_seq_len:] = 0
+
+        if replace_sensorial_data:
+            sensorial_data = sensorial_data.clone()
+
+        state = state.clone().to(device)
+        sensorial_data = sensorial_data.to(device)
+        sensorial_masks = sensorial_masks.to(device)
+
+        for i in range(start, total_size):
+            logits = self(state=state, sensorial_data=sensorial_data,
+                          sensorial_masks=sensorial_masks, input_sequence_size=i+1)
+
+            if i != total_size-1:
+                state[:, i+1] = logits["state"][:, i]
+
+                if replace_sensorial_data:
+                    for name in sensorial_data.keys():
+                        sensorial_data[name][:, i+1] = logits[name][:, i]
+
+        return logits
