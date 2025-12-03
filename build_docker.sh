f7c511fe4119c50eb91d5c61922c8298af40541e
EltonCN <eltsu3110@hotmail.com>
2025-12-03 15:05:28 -0300

    Fix docker build

--- build_docker.sh
+++ build_docker.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-pip install generate-dockerignore-from-gitignore
+pip install generate-dockerignore-from-gitignore setuptools_scm
 
 generate-dockerignore
 
-VERSION=$(git describe --tags --dirty --always)
+VERSION=$(python3 -m setuptools_scm)
+DOCKER_TAG=$(echo "$VERSION" | sed 's/[^a-zA-Z0-9._-]/-/g')
 
-docker build --build-arg "VERSION=${VERSION}" -t "eltoncn/world-machine:${VERSION}" .
+docker build --build-arg "VERSION=${VERSION}" -t "eltoncn/world-machine:${DOCKER_TAG}" .
